7be62554299d33752802b0d8516442e26e5deb0d
Clayton Wong <claytonjwong@gmail.com>
2022-03-01 09:50:11 -0700

    🚫 Low Cohension + 🚫 Tight Coupling 👉 ✅ High Cohesion + ✅ Low Coupling

--- hello-world/src/app.js
+++ hello-world/src/app.js
@@ -1,31 +1,15 @@
 (function () {
     'use strict';
 
-    angular.module('helloWorld', [])
-        .controller('helloWorldController', function ($scope) { // angular provides data binding to ng-controller in the view (HTML)
+    angular.module('nameCalculator', [])
+        .controller('nameCalculatorController', function($scope) {
             $scope.name = "✅ Angular 🙂";
+            $scope.total = $scope.name.split('').map(c => c.charCodeAt(0)).reduce((a, b) => a + b, 0);
+            $scope.keyUp = function() {
+                $scope.total = calculateNumericOutput();
+            }
+            function calculateNumericOutput() {
+                return $scope.name.split('').map(c => c.charCodeAt(0)).reduce((a, b) => a + b, 0);
+            }
         });
-
-    //
-    // 🚫 low cohesion + 🚫 tight coupling example for why we would prefer ✅ Model-View-ViewModel design pattern (MVVM) instead:
-    //
-    document.addEventListener('DOMContentLoaded', contentLoaded);
-    function contentLoaded(event) {
-        document.getElementById('name').addEventListener('keyup', keyUp);
-    }
-
-    function keyUp(event) {
-        calculateNumericOutput();
-    }
-
-    // 🚫 low cohesion: this one function does 3 separate and distinct actions...
-    // 1. reads name input from input text field from the view
-    // 2. calculates the numeric value of the name
-    // 3. writes the numeric value to the output text field of the view
-    function calculateNumericOutput() {
-        let s = document.getElementById('name').value;  // tight coupling between JS + HTML
-        let x = s.split('').map(c => c.charCodeAt(0)).reduce((a, b) => a + b);
-        let output = `Total numeric value: ${x}`;
-        document.getElementById('output').innerText = output; // tight coupling between JS + HTML
-    }
 })();
